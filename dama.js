7e6454ac7644721b3607496eba8a326f9129add9
rperiago <periago.ricardo@gmail.com>
2014-02-13 21:19:40 -0200

    experimentando

--- dama.js
+++ dama.js
@@ -9,6 +9,7 @@ var board = document.getElementById("board");
 var blackSquare = document.getElementsByClassName("white");
 var whiteSquare = document.getElementsByClassName("white"); 
 
+var Possible = new Array();
 // para funcionar no IE
 
 if (!document.getElementsByClassName) {
@@ -68,6 +69,8 @@ if (!document.getElementsByClassName) {
             sq = blackSquare[i].getAttribute("sq");   
             blackSquare[i].innerHTML+="<span class='partblack'>(0)</span>";
             blackSquare[i].setAttribute("part", p);
+            blackSquare[i].setAttribute("type", 'b');
+            blackSquare[i].setAttribute("dama", false);
             blackSquare[i].setAttribute("onclick", "game.possibleMove("+p+","+row+","+sq+",'b',this.id)");
             
             p++;
@@ -78,6 +81,8 @@ if (!document.getElementsByClassName) {
             sq = whiteSquare[i].getAttribute("sq");
             whiteSquare[i].innerHTML+="<span class='partWhite'>(0)</span>";
             whiteSquare[i].setAttribute("part", p);
+            whiteSquare[i].setAttribute("type", 'w');
+            whiteSquare[i].setAttribute("dama", false);
             whiteSquare[i].setAttribute("onclick", "game.possibleMove("+p+","+row+","+sq+",'w',this.id)");
 
             p++;
@@ -91,43 +96,115 @@ if (!document.getElementsByClassName) {
         
     }
 
+    game.ajustMove = function(sq,row,type){
+        
+
+        if (type == 'w') {
+            Possible['mLeft'] = sq-1;
+            Possible['mRight'] = sq+1;
+            Possible['mTop'] = row-1;
+            Possible['type'] = type;
+        }
+
+        if (type == 'b') {
+            Possible['mLeft'] = sq-1;
+            Possible['mRight'] = sq+1;
+            Possible['emTop'] = row+1;
+            Possible['type'] = type;
+        }
+         console.log(Possible); 
+        return Possible;
+        
+
+    }
+
     // verifica os movimentos possiveis
     game.possibleMove = function(part,row,sq,type,oldId){
 
+        //possible = game.ajustMove(sq,row,type);
+        //
+       
+        mLeft = sq-1;
+        mRight = sq+1;
         
-        if (type == 'w') {
-            mLeft = sq-1;
-            mRight = sq+1;
+        if (type == 'w') {   
             mTop = row-1;
         }
 
         if (type == 'b') {
-            mLeft = sq-1;
-            mRight = sq+1;
             mTop = row+1;
         }
-        if (sq > 1) {
-            game.clearActive();
-            select1 = document.getElementById("sq_"+mTop+"_"+mLeft);
-            part = select1.hasAttribute("part");
-            if (part != true){
-                existingClass = select1.getAttribute("class");
-                select1.setAttribute("class",existingClass+" active");
-                select1.setAttribute("onclick", "game.partMove(this.id,"+oldId+",'"+type+"')");
-            }
+         
+        //console.log("sq_"+mTop+"_"+mLeft);
+        if (sq > 1){
+            game.clearActive(); 
+            nextSquareLeft = document.getElementById("sq_"+mTop+"_"+mLeft);
+            existingClassLeft = nextSquareLeft.getAttribute("class");
+            partLeft = nextSquareLeft.hasAttribute("part");
+            nextSquareLeft.setAttribute("class",existingClassLeft+" active");
+            nextSquareLeft.setAttribute("onclick", "game.partMove(this.id,"+oldId+",'"+type+"')");
         }
-        if (sq <= 7) {
+        if (sq <= 7){
             if (sq == 1) {
-                game.clearActive(); 
-            }
-            select2 = document.getElementById("sq_"+mTop+"_"+mRight);
-            part = select2.hasAttribute("part");
-            if (part != true) {  
-                existingClass = select2.getAttribute("class");
-                select2.setAttribute("class",existingClass+" active");
-                select2.setAttribute("onclick", "game.partMove(this.id,"+oldId+",'"+type+"')");
+               game.clearActive(); 
+            } 
+            nextSquareRight = document.getElementById("sq_"+mTop+"_"+mRight);
+            existingClassRight = nextSquareRight.getAttribute("class"); 
+            partRight = nextSquareLeft.hasAttribute("part");
+            if (partRight == false) {
+                nextSquareRight.setAttribute("class",existingClassRight+" active");
+                nextSquareRight.setAttribute("onclick", "game.partMove(this.id,"+oldId+",'"+type+"')");
+            }else{
+                nextType = nextSquareRight.getAttribute('type')
+                if (nextType != type) {
+                   // mLeft = mLeft+1;
+                    //mRight = mRight+1;
+                    if (type == 'w') {   
+                        mTop = row;
+                    }
+
+                    if (type == 'b') {
+                        mTop = row;
+                    }
+
+                    nextSquareRight = document.getElementById("sq_"+mTop+"_"+mRight);
+                    nextSquareRight.setAttribute("class",existingClassRight+" active");
+                    nextSquareRight.setAttribute("onclick", "game.partMove(this.id,"+oldId+",'"+type+"')");
+                }
             }
         }
+        
+
+        //if (sq > 1) {
+            //game.clearActive();
+            //select1 = document.getElementById("sq_"+mTop+"_"+mLeft);
+            //part = select1.hasAttribute("part");
+            //if (part != true){
+                //existingClass = select1.getAttribute("class");
+                //select1.setAttribute("class",existingClass+" active");
+                //select1.setAttribute("onclick", "game.partMove(this.id,"+oldId+",'"+type+"')");
+            //}else{
+             //console.log('dir'); 
+                 
+            //}
+        //}
+        //if (sq <= 7) {
+            //if (sq == 1) {
+                //game.clearActive(); 
+            //}
+            //select2 = document.getElementById("sq_"+mTop+"_"+mRight);
+            //part = select2.hasAttribute("part");
+            
+            //if (part != true) {  
+                //existingClass = select2.getAttribute("class");
+                //select2.setAttribute("class",existingClass+" active");
+                //select2.setAttribute("onclick", "game.partMove(this.id,"+oldId+",'"+type+"')");
+            //} else {
+                //console.log('esq');
+               
+            //}
+           //if(select2.getAttribute('type') == type){ 
+        //}
 
     }
 
@@ -145,6 +222,8 @@ if (!document.getElementsByClassName) {
         // limpa os possivei movimentos
         game.clearActive(); 
         old.removeAttribute("onclick");
+        old.removeAttribute("type"); 
+        old.removeAttribute("dama"); 
         
          // seta o id da peça em seu novo lugar
         move.setAttribute("part", part);
@@ -158,16 +237,19 @@ if (!document.getElementsByClassName) {
         if (type == "w") {
             move.setAttribute("onclick", "game.possibleMove("+part+","+row+","+sq+",'w',this.id)"); 
             move.innerHTML+="<span class='partWhite'>(0)</span>";
-            move.setAttribute('teste','123');
+            move.setAttribute("type", 'w');
+            move.setAttribute("dama", false);
+
             
         }else{
             move.innerHTML+="<span class='partBlack'>(0)</span>";
             move.setAttribute("onclick", "game.possibleMove("+part+","+row+","+sq+",'b',this.id)");
-            move.setAttribute('teste','123');
+            move.setAttribute("type", 'b');
+            move.setAttribute("dama", false); 
         }
         // limpa o guadrado anterior
         old.innerHTML="";
-        
+        old.removeAttribute("part");
     }
 
     // inicializa
@@ -178,3 +260,22 @@ if (!document.getElementsByClassName) {
     return game.start();
 
 })();
+
+  //tipo = select1.getAttribute('type');
+                //if (tipo != 'w') {
+                    //mLeft = mLeft-1;
+                    //mRight = mRight+1;
+                    //mTop = mTop-1;
+                //}
+
+                //if (tipo != 'b') {
+                    //mLeft = mLeft-1;
+                    //mRight = mRight+1;
+                    //mTop = mTop+1;
+                //}
+                
+                //console.log('entro');
+                //console.log(tipo);
+                //nSql = document.getElementById("sq_"+mTop+"_"+mLeft);
+                //nSql.setAttribute("class",existingClass+" active");
+                //nSql.setAttribute("onclick", "game.partMove(this.id,"+oldId+",'"+type+"')");  
