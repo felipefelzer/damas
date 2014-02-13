79ea2edfdef1e1f2103e908ca6eb79046fb8f6d4
rperiago <periago.ricardo@gmail.com>
2014-02-12 22:11:20 -0200

    Aplicado movimentos possiveis para as peças pretas

--- dama.js
+++ dama.js
@@ -7,12 +7,13 @@
 
 var game = {};
 var board = document.getElementById("board"); 
-
+var blackSquare = document.getElementsByClassName("black");
+var whiteSquare = document.getElementsByClassName("white"); 
 
 // para funcionar no IE
 
 if (!document.getElementsByClassName) {
-        document.getElementsByClassName=function(cn) {
+    document.getElementsByClassName=function(cn) {
         var allT=document.getElementsByTagName('*'), allCN=[], i=0, a;
         while(a=allT[i++]) {
             a.className==cn ? allCN[allCN.length]=a : null;
@@ -26,49 +27,50 @@ if (!document.getElementsByClassName) {
 
 (function(){
 
+    // fazendo o quadrados pretos e brancos alternados
     game.squareColor = function(r,c){
-       
-        color = Math.abs((r+c)%2)
-        
+
+        color = Math.abs((r+c)%2);
+
         if (color > 0) {
             color = "black";
-        }else{
+        }
+        else{
             color = "white";
         }
 
         return color;
     }
-
+    //distribui os quadrados
     game.makeSquare = function(r){
 
         var square = document.getElementById("row_"+r);
-        
+
         for (s = 1; s < 9; s++) {
             square.innerHTML+="<div id='sq_"+r+"_"+s+"' class='square "+game.squareColor(r,s)+"' row='"+r+"' sq='"+s+"'></div>";
-            //game.squareColor(r,s);
         }
 
     }
-
+    //monta o tabuleiro
     game.table = function(){
         for (i = 1; i < 9; i++) {
             board.innerHTML+="<div id='row_"+i+"' class='row' row='"+i+"'></div>";
             game.makeSquare(i);
-            
+
         }
-       
+
     }
 
+    // ditribui peças 
     game.takePart = function(){
-        
-        var blackSquare = document.getElementsByClassName("black");
-        var whiteSquare = document.getElementsByClassName("white"); 
-        
-        // ditribui peças 
         p = 1;
         for (var i = 0; i < 12; i++) {
+            row = blackSquare[i].getAttribute("row");
+            sq = blackSquare[i].getAttribute("sq");   
             blackSquare[i].innerHTML+="(0)";
             blackSquare[i].setAttribute("part", p);
+            blackSquare[i].setAttribute("onclick", "game.possibleMove("+p+","+row+","+sq+",'b')");
+            
             p++;
         }
         p = 1;
@@ -77,33 +79,55 @@ if (!document.getElementsByClassName) {
             sq = whiteSquare[i].getAttribute("sq");
             whiteSquare[i].innerHTML+="(0)";
             whiteSquare[i].setAttribute("part", p);
-            whiteSquare[i].setAttribute("onclick", "game.possibleMove("+p+","+row+","+sq+")");
-     
+            whiteSquare[i].setAttribute("onclick", "game.possibleMove("+p+","+row+","+sq+",'w')");
+
             p++;
         }
     }
-
-    game.possibleMove = function(part,row,sq){
-        //.getAttributeNode("target");
-       mLeft = sq-1;
-       mRight = sq+1;
-       mTop = row-1;
-       if (sq > 1) {
-           select1 = document.getElementById("sq_"+mTop+"_"+mLeft);
-       }
-       if (sq != 8) {
-           select2 = document.getElementById("sq_"+mTop+"_"+mRight);
-       }
-       console.log(mLeft);
-       console.log(mRight);
-       console.log(mTop);
-       //select1.getAttribute("class")
-       existingClass = select1.getAttribute("class");
-       select1.setAttribute("class",existingClass+" active");
-
-       select2.setAttribute("class",existingClass+" active");
+    // limpa a class active
+    game.clearActive = function (){
+        for (var i = 0; i < whiteSquare.length; i++) {
+            whiteSquare[i].setAttribute("class","square white");
+        };
+        for (var i = 0; i < blackSquare.length; i++) {
+            blackSquare[i].setAttribute("class","square black");
+        }; 
     }
 
+    // verifica os movimentos possiveis
+    game.possibleMove = function(part,row,sq,type){
+        if (type == 'w') {
+            mLeft = sq-1;
+            mRight = sq+1;
+            mTop = row-1;
+        }
+
+        if (type == 'b') {
+            mLeft = sq-1;
+            mRight = sq+1;
+            mTop = row+1;
+        }
+        if (sq > 1) {
+            game.clearActive();
+            select1 = document.getElementById("sq_"+mTop+"_"+mLeft);
+            part = select1.hasAttribute("part");
+            if (part != true){
+                existingClass = select1.getAttribute("class");
+                select1.setAttribute("class",existingClass+" active");
+            }
+        }
+        if (sq <= 7) {
+            select2 = document.getElementById("sq_"+mTop+"_"+mRight);
+            part = select2.hasAttribute("part");
+            if (part != true) {  
+                existingClass = select2.getAttribute("class");
+                select2.setAttribute("class",existingClass+" active");
+            }
+        }
+
+    }
+
+    // inicializa
     game.start = function(){
         game.table();
         game.takePart();
